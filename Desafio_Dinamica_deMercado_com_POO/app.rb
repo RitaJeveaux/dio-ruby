9c4188d1fd1f44ac23e35dad38fc216297c7e208
ritajeveaux <69821551+RitaJeveaux@users.noreply.github.com>
2023-10-07 11:43:54 -0300

    Rename Dinamica_de_Mercado_com_POO directory

--- Desafio_Dinamica_deMercado_com_POO/app.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'produto'
-require_relative 'mercado'
-
-produto = Produto.new
-    produto.nome = 'Forma de Bolo'
-    produto.preco = 36.09
-
-Mercado.new(produto.nome, produto.preco).comprar

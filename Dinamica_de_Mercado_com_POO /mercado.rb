881eb0bb7ba08dfaa4a105c7ce1b8b4296ab6534
ritajeveaux <69821551+RitaJeveaux@users.noreply.github.com>
2023-10-07 11:49:16 -0300

    Update mercado.rb

--- Dinamica_de_Mercado_com_POO /mercado.rb
+++ Dinamica_de_Mercado_com_POO /mercado.rb
@@ -1,1 +1,12 @@
+class Mercado
 
+    def initialize(produto, preco)
+        @produto = produto
+        @preco = preco
+    end
+
+    def comprar
+        puts "Você comprou o produto #{@produto} no valor de R$#{@preco} "
+    end
+
+end

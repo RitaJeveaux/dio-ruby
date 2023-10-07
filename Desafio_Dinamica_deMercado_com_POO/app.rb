e5e40594a591f5299840c960897ac417833faaf7
ritajeveaux <69821551+RitaJeveaux@users.noreply.github.com>
2023-10-07 11:41:56 -0300

    Update app.rb

--- Desafio_Dinamica_deMercado_com_POO/app.rb
+++ Desafio_Dinamica_deMercado_com_POO/app.rb
@@ -1,1 +1,8 @@
+require_relative 'produto'
+require_relative 'mercado'
 
+produto = Produto.new
+    produto.nome = 'Forma de Bolo'
+    produto.preco = 36.09
+
+Mercado.new(produto.nome, produto.preco).comprar
